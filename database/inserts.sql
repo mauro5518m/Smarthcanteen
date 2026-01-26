43c1dc1591aeb62a694d7a507fc59081eed0be54
mauro5518m <piresmauro738@gmail.com>
2026-01-26 18:08:26 -0100

    ETAPA 1 - Base de Dados

--- database/inserts.sql
+++ database/inserts.sql
@@ -1,2 +1,7 @@
 -- dados iniciais do sistema
+INSERT INTO utilizador (tipo, nome, numero, curso_departamento, email, saldo)
+VALUES ('funcionario','Admin','F001','Serviços','admin@um.edu.cv',0);
+
+INSERT INTO refeicao (nome, categoria, preco, quantidade_diaria)
+VALUES ('Prato do Dia','Almoço',250,50);
 
