1265c88230369a266b244e0c3551f786f24ecc90
mauro5518m <piresmauro738@gmail.com>
2026-01-26 18:59:36 -0100

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,15 +1,1 @@
-import mysql.connector
-
-def conectar():
-    return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="12345",  # se tu definiste senha, coloca aqui
-        database="localhost"
-    )
-
-# teste de conexão
-if __name__ == "__main__":
-    conn = conectar()
-    print("Conectado:", conn.is_connected())
 
