10a513de599b5920d86ebfccceaaaa4436f3c542
mauro5518m <piresmauro738@gmail.com>
2026-01-29 22:44:26 -0100

    Update compras.py

--- src/compras.py
+++ src/compras.py
@@ -1,1 +1,17 @@
 # compras de refeições
+from src.db import conectar
+
+def comprar(id_estudante, id_refeicao, quantidade=1):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO compra (id_utilizador, id_refeicao, quantidade) VALUES (%s, %s, %s)",
+        (id_estudante, id_refeicao, quantidade)
+    )
+    conn.commit()
+
+# teste rápido
+if __name__ == "__main__":
+    comprar(1, 1)
+    print("compra realisado com sucesso")
+
