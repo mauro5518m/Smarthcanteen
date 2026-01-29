bf0750de75254e6c6e192828a225ecd3149c27cd
mauro5518m <piresmauro738@gmail.com>
2026-01-29 22:44:58 -0100

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,1 +1,15 @@
+import mysql.connector
+
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="12345",  # tua senha do MySQL
+        database="smartcanteen"
+    )
+
+# teste rápido
+if __name__ == "__main__":
+    conn = conectar()
+    print("Conectado:", conn.is_connected())
 
