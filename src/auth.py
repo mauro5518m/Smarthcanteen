dcaf7ec27c5fcdbe7731874f966be688e580af83
mauro5518m <piresmauro738@gmail.com>
2026-01-29 22:43:07 -0100

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,1 +1,19 @@
 # autenticação de funcionários
+from src.db import conectar
+
+def login(email):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM utilizador WHERE email=%s AND tipo='funcionario'",
+        (email,)
+    )
+    return cursor.fetchone()
+
+# teste rápido
+if __name__ == "__main__":
+    usuario = login("admin@um.edu.cv")
+    if usuario:
+        print("Login com sucesso:", usuario)
+    else:
+        print("Login falhou")
